bc9178b7e9ce04b5ad20a94b6f20c5db6a4410f6
marcimerca <124384247+marcimerca@users.noreply.github.com>
2023-04-18 10:58:51 +0200

    team card example

--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -4,8 +4,45 @@
             Teams
         </h1>
     </div>
+    <div class="teams-container">
+
+        <div class="team-card">
+            <img class="logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Logo_of_AC_Milan.svg/1306px-Logo_of_AC_Milan.svg.png">
+            <h2>AC Milan</h2>
+        </div>
+
+
+    </div>
 </template>
 
-<style scoped>
 
+<style scoped>
+.team-card {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid black;
+    padding: 18px;
+    width: fit-content;
+    margin: 0 auto;
+    cursor: pointer;
+
+}
+
+.logo {
+    width: 90px;
+    margin-right: 10px;
+}
+
+.team-photo {
+    width: 30%;
+    height: 30%;
+
+}
+
+.team-card h2 {
+    font-size: 30px;
+    font-weight:600;
+}
 </style>
