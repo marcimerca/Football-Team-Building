1cb1ba8f1feba1193e0456f86ad9ef9254e529f7
marcimerca <124384247+marcimerca@users.noreply.github.com>
2023-04-19 19:12:34 +0200

    defined props and style

--- src/views/PlayerDetails.vue
+++ src/views/PlayerDetails.vue
@@ -1,25 +1,59 @@
 <script setup>
-import players from "../data/players.json"
+  import players from "../data/players.json";
 
-const props = defineProps({
+  const props = defineProps({
     playerId: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-});
+  });
 
-const player = players.find((player) => player.playerId === props.playerId)
-
+  const player = players.find((player) => player.playerId === props.playerId);
 </script>
 
-
 <template>
-    <header>
-        <h1>
-            Player Details
-        </h1>
-    </header>
-    <img :src="player.playerImage">
+  <header>
+    <h1>
+      {{ player.playerName }}
+    </h1>
+  </header>
+  <v-card width="500px">
+    <v-img cover height="250" :src="player.playerCover"></v-img>
+    <v-card-item>
+      <v-card-title>{{ player.playerPosition }}</v-card-title>
+      <v-card-subtitle>
+        <span class="me-1">#{{ player.playerNumber }}</span>
+      </v-card-subtitle>
+      <v-card-item class="info">
+        <v-card-item class="key-value">
+          <v-chip> Date of birth </v-chip>
+          <v-card-text class="card-text">
+            {{ player.playerDateOfBirth }}</v-card-text
+          >
+        </v-card-item>
+        <v-card-item class="key-value">
+          <v-chip>Height</v-chip>
+          <v-card-text class="card-text">{{ player.playerHeight }}</v-card-text>
+        </v-card-item>
+        <v-card-item class="key-value">
+          <v-chip>Nationality</v-chip>
+          <v-card-text class="card-text">{{
+            player.playerNationality
+          }}</v-card-text>
+        </v-card-item>
+      </v-card-item>
+    </v-card-item>
+  </v-card>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+header {
+    margin-bottom: 15px;
+}
+  .card-text {
+    padding: 2px;
+  }
+
+
+</style>
