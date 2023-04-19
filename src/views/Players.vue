84ad7a3f2e200444824166347e401fd8262383e9
marcimerca <124384247+marcimerca@users.noreply.github.com>
2023-04-19 17:59:31 +0200

    added routes to player details

--- src/views/Players.vue
+++ src/views/Players.vue
@@ -1,5 +1,8 @@
 <script setup>
   import players from "../data/players.json";
+  import { useRouter } from "vue-router";
+const router = useRouter();
+  const goToPlayer = (playerId) => router.push(`/players/${playerId}`)
 </script>
 
 <template>
@@ -7,19 +10,19 @@
     <h1>List of players</h1>
   </header>
   <v-list class="list" lines="one">
-    <v-list-item
+    <v-list-item @click="goToPlayer(player.playerId)"
       v-for="player in players"
       :key="player.playerId"
       :title="player.playerName"
       :subtitle="player.playerPosition"
       :prepend-avatar="player.playerImage"
     >
-   </v-list-item>
+    </v-list-item>
   </v-list>
 </template>
 
 <style scoped>
-header {
-  margin-bottom: 30px;
-}
+  header {
+    margin-bottom: 30px;
+  }
 </style>
