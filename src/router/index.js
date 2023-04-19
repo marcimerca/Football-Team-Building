91d29a6ebf7672226ee9bf0507480eff0db216b2
marcimerca <124384247+marcimerca@users.noreply.github.com>
2023-04-19 10:40:49 +0200

    route for player-details

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Home from "../views/Home.vue";
 import Teams from "../views/Teams.vue";
 import TeamDetails from "../views/TeamDetails.vue";
 import Players from "../views/Players.vue";
+import PlayerDetails from "../views/PlayerDetails.vue";
+
 import NotFound from "../views/NotFound.vue";
 
 const router = createRouter({
@@ -31,6 +33,11 @@ const router = createRouter({
       path: "/players",
       name: "players",
       component: Players,
+    },
+    {
+      path: "/players/:playerId",
+      name: "player-details",
+      component: PlayerDetails,
     },
     {
       path: "/:pathMatch(.*)*",
---
+++ src/views/PlayerDetails.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+
+<template>
+    <div>
+        <h1>
+            Player Details
+        </h1>
+    </div>
+</template>
+
+<style scoped></style>
