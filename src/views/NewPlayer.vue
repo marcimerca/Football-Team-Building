3eee0404618ac6c0293ce98c76f5f5826ce608f5
marcimerca <124384247+marcimerca@users.noreply.github.com>
2023-04-20 18:56:47 +0200

    add form

--- src/views/NewPlayer.vue
+++ src/views/NewPlayer.vue
@@ -1,5 +1,54 @@
+<script setup>
+import { Database } from "../services/database";
+import { ref } from "vue";
+const players = ref([]);
+const getPlayers = async () => {
+  players.value = await Database.Players.get();
+};
+getPlayers();
+</script>
+
 <template>
   <header>
     <h1>New Player</h1>
   </header>
+  <div class="form-container">
+    <v-text-field
+      v-model="name"
+      class="input"
+      clearable
+      label="Name"
+    ></v-text-field>
+    <v-select
+      class="input"
+      label="Position"
+      :items="['GoalKeeper', 'Defender', 'Midfielder', 'Forward']"
+    ></v-select>
+    <v-text-field class="input" clearable label="Nationality"></v-text-field>
+    <v-text-field
+      class="input"
+      type="number"
+      clearable
+      suffix="m"
+      label="Height"
+      model-value="1.50"
+    ></v-text-field>
+  </div>
 </template>
+
+<style scoped>
+header {
+  margin-bottom: 30px;
+}
+.form-container {
+  display: flex;
+  gap: 20px;
+  flex-wrap: wrap;
+  width: 100%;
+  font-size: 4px;
+}
+
+.input {
+  width: 60%;
+}
+</style>
