6b526e744ef0790073abacfbaf04c4559303f037
marcimerca <124384247+marcimerca@users.noreply.github.com>
2023-04-29 00:10:34 +0200

    add style to home cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,163 +7,100 @@
     <div class="info">
       <v-card class="card">
         <v-card-item>
-          <v-card-actions>
-            <v-card-item class="button-container">
-              <v-btn
-                @click="showAbout = true"
-                prepend-icon="md:expand_more"
-                color="black"
-                variant="text"
+          <v-card-item color="black">
+            <h2>About</h2>
+          </v-card-item>
+
+          <v-card-text>
+            <p>
+              This project was created using
+              <strong
+                ><a href="https://vuejs.org/" target="”_blank”"
+                  ><code>Vue.js</code></a
+                ></strong
               >
-                <h2>About</h2>
-              </v-btn>
-            </v-card-item>
-          </v-card-actions>
-          <v-expand-transition>
-            <div v-show="showAbout">
-              <v-card-text>
-                <p>
-                  This project was created using
-                  <strong
-                    ><a href="https://vuejs.org/" target="”_blank”"
-                      ><code>Vue.js</code></a
-                    ></strong
-                  >
-                  as the main framework. For the style, in addition to CSS, the
-                  <a href="https://vuetifyjs.com/en/" target="”_blank”"
-                    ><code>Vuetify</code></a
-                  >
-                  library has been imported. In this project it will be possible
-                  to view three main pages: <em>Home</em>, <em>Teams</em>, and
-                  <em>Players.</em>
-                </p>
-                <br />
-                <ul>
-                  <li>
-                    In <em>Home</em>, you can view this brief presentation of
-                    the project with the main information about it.
-                  </li>
-                  <li>
-                    In <em>Teams</em>, you can view three italian football
-                    clubs, Milan, Juventus and Inter. From there, you can access
-                    to a page with more details about each team.
-                  </li>
-                  <li>
-                    In <em>Players</em>, you can view a list of players
-                    belonging to the teams mentioned above. From there, you can
-                    access to a page with more details about each player and to
-                    the page where you can create a new player.
-                  </li>
-                </ul>
-              </v-card-text>
-            </div>
-          </v-expand-transition>
-        </v-card-item>
-        <v-card-item class="footer-button">
-          <v-btn
-            v-if="showAbout"
-            @click="showAbout = false"
-            text-align="center"
-            variant="outlined"
-          >
-            Close
-          </v-btn>
+              as the main framework. For the style, in addition to CSS, the
+              <a href="https://vuetifyjs.com/en/" target="”_blank”"
+                ><code>Vuetify</code></a
+              >
+              library has been imported. In this project it will be possible to
+              view three main pages: <em>Home</em>, <em>Teams</em>, and
+              <em>Players.</em>
+            </p>
+            <br />
+            <ul>
+              <li>
+                In <em>Home</em>, you can view this brief presentation of the
+                project with the main information about it.
+              </li>
+              <li>
+                In <em>Teams</em>, you can view three italian football clubs,
+                Milan, Juventus and Inter. From there, you can access to a page
+                with more details about each team.
+              </li>
+              <li>
+                In <em>Players</em>, you can view a list of players belonging to
+                the teams mentioned above. From there, you can access to a page
+                with more details about each player and to the page where you
+                can create a new player.
+              </li>
+            </ul>
+          </v-card-text>
         </v-card-item>
       </v-card>
     </div>
     <div class="why-who">
       <v-card class="card">
-        <v-card-item>
-          <v-card-actions>
-            <v-card-item class="button-container">
-              <v-btn
-                @click="showWhy = true"
-                prepend-icon="md:expand_more"
-                color="black"
-                variant="text"
-              >
-                <h2>Why ?</h2>
-              </v-btn>
-            </v-card-item>
-          </v-card-actions>
-          <v-expand-transition>
-            <div v-show="showWhy">
-              <v-card-text>
-                <p>
-                  I decided to undertake this project with the aim of putting
-                  into practice the knowledge acquired so far in my web
-                  developing career.
-                </p>
-              </v-card-text>
-            </div>
-          </v-expand-transition>
-          <v-card-item class="footer-button">
-            <v-btn v-if="showWhy" @click="showWhy = false" variant="outlined">
-              Close
-            </v-btn>
+        <v-card-item class="why-who-cont">
+          <v-card-item>
+            <h2>Why ?</h2>
           </v-card-item>
-          <v-divider :thickness="1" color="black"></v-divider>
 
-          <v-card-actions>
-            <v-card-item class="button-container">
-              <v-btn
-                @click="showWho = true"
-                color="black"
-                prepend-icon="md:expand_more"
-                variant="text"
-              >
-                <h2>Who ?</h2>
-              </v-btn>
-            </v-card-item>
-          </v-card-actions>
-          <v-expand-transition>
-            <div v-show="showWho">
-              <v-card-text>
-                <p>
-                  My name is Marcello Mercanzin, i'm 26 and i live near Vicenza.
-                  I've always been interested in the world of web development,
-                  and now I would like to pursue this type of career.
-                </p>
-              </v-card-text>
-            </div>
-          </v-expand-transition>
-        </v-card-item>
-        <v-card-item class="footer-button">
-          <v-btn v-if="showWho" @click="showWho = false" variant="outlined">
-            Close
-          </v-btn>
+          <v-card-text>
+            <p>
+              I decided to undertake this project with the aim of putting into
+              practice the knowledge acquired so far in my web developing
+              career. In particular, I wanted to create a project related to the
+              world of football, as it is one of my passions.
+            </p>
+          </v-card-text>
+          <v-divider :thickness="1" color="black"></v-divider>
+          <v-card-item>
+            <h2>Who ?</h2>
+          </v-card-item>
+          <v-card-text>
+            <p>
+              My name is Marcello Mercanzin, i'm 26 and i live near Vicenza.
+              I've always been interested in the world of web development, and
+              now I would like to pursue this type of career.
+            </p>
+          </v-card-text>
         </v-card-item>
       </v-card>
     </div>
     <div class="explore">
-      <v-card class="card">
-        <v-card-item class="explore-container">
-          <v-btn variant="text">
+      <v-card class="explore-card">
+        <v-card-item>
+          <v-card-item color="black">
             <h2>Explore</h2>
-          </v-btn>
-        </v-card-item>
-        <v-spacer></v-spacer>
-        <v-card-text>
-          <p>
-            Click one of the button in the Navigation Bar to start exploring
-            this project.
-          </p>
-        </v-card-text>
-        <v-card-item class="ball">
-          <v-icon icon="md:home" size="25px" style="margin: 3px" />
-          <v-icon icon="md:sports_soccer" style="margin: 3px" size="25px" />
-          <v-icon icon="md:groups" style="margin: 3px" size="25px" />
+          </v-card-item>
+          <v-card-text>
+            <p>
+              Click one of the button in the Navigation Bar to start exploring
+              this project.
+            </p>
+          </v-card-text>
+          <v-card-item class="ball">
+            <v-icon icon="md:home" size="25px" style="margin: 3px" />
+            <v-icon icon="md:sports_soccer" style="margin: 3px" size="25px" />
+            <v-icon icon="md:groups" style="margin: 3px" size="25px" />
+          </v-card-item>
         </v-card-item>
       </v-card>
     </div>
   </div>
 </template>
-<script setup>
-import { ref } from "vue";
-const showAbout = ref(false);
-const showWhy = ref(false);
-const showWho = ref(false);
-</script>
+
 <style scoped>
 header {
   color: rgb(236, 236, 236);
@@ -184,40 +121,42 @@ header {
   margin: 0 auto;
   width: 30%;
   text-align: left;
-  margin-bottom: 15px;
 }
 
 .card {
   background-color: rgb(236, 236, 236);
+  height: 450px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+.explore-card {
+  background-color: rgb(236, 236, 236);
+  height: 225px;
+  display: flex;
+  flex-direction: column;
+  margin-top: 0;
 }
 
+.explore-card h2 {
+  margin-top: 10px;
+}
 .info {
   height: 50%;
   width: 33%;
 }
-.button-container {
-  margin: 0 auto;
-  display: flex;
-  align-items: center;
-}
-.explore-container {
-  text-align: center;
-  margin-top: 17px;
-}
+
 a {
   margin: 0 10px;
   color: black;
 }
 .ball {
   text-align: center;
+  width: 100px;
   margin: 0 auto;
-}
-.footer-button {
-  width: fit-content;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  text-align: center;
-  margin: 0 auto;
 }
 
 /*Mobile version*/
