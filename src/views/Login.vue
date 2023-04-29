dd08335ec0a9cbd536c3ecf993817dd8caaaa128
marcimerca <124384247+marcimerca@users.noreply.github.com>
2023-04-29 17:16:37 +0200

    add image

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,16 +3,22 @@
     <v-btn @click="login">Login With Google</v-btn>
   </div>
   <div v-else>
-    <h3 style="margin-bottom: 30px">
-      Welcome {{ userStore.user.displayName }}
-    </h3>
+    <h3>Welcome</h3>
+    <h3 style="margin-bottom: 2px">{{ userStore.user.displayName }}</h3>
+    <v-img style="margin-bottom: 2px" :src="playerwhite"></v-img>
     <v-btn @click="logout">Logout</v-btn>
   </div>
 </template>
 
 <script setup>
+import playerwhite from "../images/playerwhite.png";
 import { login, logout } from "../services/auth";
 import { useUserStore } from "../store/user";
 
 const userStore = useUserStore();
 </script>
+<style scoped>
+h3 {
+  color: rgb(236, 236, 236);
+}
+</style>
