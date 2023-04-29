8b11936b6ac0f32431fb0a8a5c0e7c857e8c9c8c
marcimerca <124384247+marcimerca@users.noreply.github.com>
2023-04-29 17:18:35 +0200

    changed nav bar bg colour

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -10,7 +10,7 @@ const userStore = useUserStore();
 <template>
   <div class="container">
     <v-layout>
-      <v-app-bar class="nav-bar">
+      <v-app-bar class="nav-bar" color="rgb(0, 0, 0, 0.5)">
         <div class="buttons">
           <v-btn @click="router.push(`/`)">
             <v-icon icon="md:home" style="margin-right: 5px" />Home
@@ -23,24 +23,33 @@ const userStore = useUserStore();
             <v-icon icon="md:groups" style="margin-right: 5px" />
             Players</v-btn
           >
-          <v-btn v-if="userStore.user" @click="router.push(`/account`)">
+          <v-btn
+            color="white"
+            v-if="userStore.user"
+            @click="router.push(`/account`)"
+          >
             <v-icon icon="md:person" style="margin-right: 5px" />{{
               userStore.user.displayName
             }}</v-btn
           >
         </div>
-        <v-btn v-if="userStore.user" @click="logout" style="margin-right: 10px">
+        <v-btn
+          color="white"
+          v-if="userStore.user"
+          @click="logout"
+          style="margin-right: 10px"
+        >
           <v-icon icon="md:logout" style="margin-right: 5px" />
           Logout
         </v-btn>
-        <v-btn v-else @click="router.push('/login')">
+        <v-btn color="white" v-else @click="router.push('/login')">
           <v-icon icon="md:login" style="margin-right: 5px" />
           Login
         </v-btn>
       </v-app-bar>
     </v-layout>
     <v-layout>
-      <v-app-bar class="mobile-bar">
+      <v-app-bar class="mobile-bar" color="rgb(0, 0, 0, 0.5)">
         <div class="buttons">
           <v-btn @click="router.push(`/`)">
             <v-icon icon="md:home" style="margin-right: 5px" size="25px" />
@@ -72,7 +81,7 @@ const userStore = useUserStore();
           <v-icon icon="md:logout" style="margin-right: 2px" size="25px" />
         </v-btn>
         <v-btn class="right-button" v-else @click="router.push('/login')">
-          <v-icon icon="md:login" style="margin-right: 2px" size="25px" />Log
+          <v-icon icon="md:login" style="margin-right: 2px" size="25px" />Login
         </v-btn>
       </v-app-bar>
     </v-layout>
@@ -83,9 +92,13 @@ const userStore = useUserStore();
 .container {
   margin-bottom: 50px;
 }
+.nav-bar {
+  border-bottom: 0.1px solid rgb(164, 161, 161);
+}
 
 .buttons {
   margin: 0 auto;
+  color: white;
 }
 
 .mobile-bar {
@@ -100,15 +113,16 @@ const userStore = useUserStore();
   .mobile-bar {
     display: inline-flex;
     height: 4rem;
-    background-color: white;
-    z-index: 900;
+    border-bottom: 0.1px solid rgb(164, 161, 161);
   }
   .buttons {
     width: 60%;
+    color: white;
   }
   .right-button {
     text-align: right;
     display: flex;
+    color: white;
   }
 }
 </style>
